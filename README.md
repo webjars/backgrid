f6e2a8608ce2dffd2299c3c334d946aa49c6e440
James Ward <james@jamesward.org>
2014-02-03 18:26:34 -0700

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+WebJar for Backgrid.js
+
+More info: http://webjars.org
+
+Upstream: http://backgridjs.com/
---
+++ src/main/resources/webjars-requirejs.js
@@ -0,0 +1,7 @@
+/*global requirejs */
+
+requirejs.config({
+  shim: {
+    'backgrid': [ 'webjars!jquery.js', 'webjars!underscore.js', 'webjars!backbone.js' ]
+  }
+});
